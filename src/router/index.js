857ff7da9aa0765914c7ff1d7e6cc33c2e6f5521
zuihoudefangzong <58388720@qq.com>
2022-06-10 14:11:54 +0800

    search组件完成header小部分

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,11 @@ const routes = [
     path: '/profile',
     name: 'Profile',
     component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: () => import('../views/Search.vue')
   }
 ]
 
---
+++ src/views/Search.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+
+    <!-- header -->
+    <header class="header">
+      <div class="search-input">
+        <i class="iconfont icon-icon-test"></i>
+        <input type="text" placeholder="请输入商品名称">
+        <i class="iconfont icon-shanchu"></i>
+        <span class="cancel">取消</span>
+      </div>
+    </header>
+
+    <!-- suggest搜索联系区域 -->
+    <section class="suggest"></section>
+
+    <!-- 历史记录search-history区域section -->
+    <section class="search-history"></section>
+
+    <!-- search-result结果 -->
+    <section class="result"></section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search'
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  background: $colorA;
+  padding: 0 .2rem;
+}
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: .1rem .2rem;
+  box-sizing: border-box;
+  .search-input {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .iconfont {
+      font-size: $fontC;
+      color: $colorC;
+    }
+    input {
+      flex: 1;
+      background-color: $colorE;
+      margin: 0 .2rem;
+      padding: .1rem .2rem;
+      border: none;
+      outline: none;
+
+    }
+    .cancel {
+      margin-left: 0.3rem;
+    }
+  }
+
+}
+</style>
