0d29ec14aa6a9413bcc5b04e3cf6b27f81aae7bf
Jianyu Wang <58388720@qq.com>
2022-06-16 18:29:37 +0800

    toast组件完成未测试

---
+++ src/components/toast/index.js
@@ -0,0 +1,42 @@
+import Main from './Main.vue'
+import Vue from 'vue'
+// 页面只能有一个toast
+let currentToast
+const toast = () => {
+  Vue.prototype.$toast = (options) => {
+    options = options || {}
+    // 假如已经有toast了 调用里面方法 销毁组件
+    if (currentToast) {
+      currentToast.close()
+      currentToast = null
+    }
+    currentToast = createToast(Vue)
+    if (options === 'sring') {
+      currentToast.msg = options
+    } else {
+      currentToast.msg = options.msg
+      currentToast.btnshow = options.btnshow
+      currentToast.visible = options.visible
+      currentToast.callback = options.callback
+    }
+  }
+}
+
+// 要构造vue组件 必要vue构造函数传过来
+function createToast (Vue) {
+  // 构造1个vue组件
+  const ToastConstructor = Vue.extend(Main)
+  // 实例化
+  const toast = new ToastConstructor()
+  // 内部插槽可以放任何html或者vue组件
+  // 可以去看container组件isVertical()
+  // toast.$slots.default = [message]
+  // Vue将toast组件挂载mount到browser内存中
+  toast.$mount()
+  // 最后才插入该dom
+  // toast.$on('close',onClick)
+  document.body.appendChild(toast.$el)
+  return toast
+}
+
+export default toast
---
+++ src/components/toast/main.vue
@@ -0,0 +1,94 @@
+<template>
+  <transition @afterleave="handleAfterLeave">
+    <!-- 遮罩层 -->
+    <div class="container" v-show="visible">
+      <div class="toast">
+        <div class="msg">{{msg}}</div>
+        <div class="btn" v-show="btnShow">
+          <div class="btn-cancel" @click="cancel">取消</div>
+          <div class="btn-confirm" @click="confirm">确认</div>
+      </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // 是否显示toast
+      visible: false,
+      // 是否显示按钮group
+      btnShow: false,
+      // 回调函数
+      callback: undefined
+    }
+  },
+  watch: {
+    visible () {
+      // 一秒后自动关闭条件
+      if (this.visible && !this.btnShow) {
+        setTimeout(() => {
+          this.visible = false
+        }, 1000)
+      }
+    }
+  },
+  methods: {
+    cancel () {
+      this.visible = false
+    },
+    confirm () {
+      // 正在关闭
+      if (this.callback && this.callback === 'function') {
+        this.callback(this)
+      }
+    },
+    // transition离开时候钩子函数afterleave的回调
+    handleAfterLeave () {
+      // this.$el.remove()
+      this.$destroy(true)
+      // 从浏览器删除改dom元素
+      this.$el.parentNode.removeChild(this.$el)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1;
+  .toast {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: rgba(7, 17, 27, 0.8);
+    border-radius: 0.1rem;
+    color: $colorA;
+    padding: 0.2rem 0.6rem;
+    text-align: center;
+    .btn {
+      height: .8rem;
+      display: flex;
+      align-items: center;
+      color: $colorA;
+      margin-top: .4rem;
+      border-top: 1px solid $colorA;
+      div {
+        flex: 1;
+        width: 2rem;
+      }
+      &-cancel {
+        border-right: 1px solid $colorA;
+      }
+    }
+  }
+}
+</style>
