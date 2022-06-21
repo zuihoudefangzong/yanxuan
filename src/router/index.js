59de1ca3180a41c35f72095f7b377639233e09ba
Jianyu Wang <58388720@qq.com>
2022-06-21 17:40:15 +0800

    每次router跳转的document.title

--- src/router/index.js
+++ src/router/index.js
@@ -8,20 +8,27 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: '首页'
+    }
   },
   {
     path: '/category',
     name: 'Categroy',
     component: () => import('../views/Category.vue'),
     meta: {
-      keepAlive: true
+      keepAlive: true,
+      title: '分类'
     }
   },
   {
     path: '/cart',
     name: 'Cart',
-    component: () => import('../views/Cart.vue')
+    component: () => import('../views/Cart.vue'),
+    meta: {
+      title: '购物车'
+    }
   },
   {
     path: '/profile',
@@ -31,39 +38,60 @@ const routes = [
       {
         path: 'login',
         name: 'Login',
-        component: () => import('../views/Login.vue')
+        component: () => import('../views/Login.vue'),
+        meta: {
+          title: '登录'
+        }
       },
       {
         path: 'regist',
         name: 'Regist',
-        component: () => import('../views/Regist.vue')
+        component: () => import('../views/Regist.vue'),
+        meta: {
+          title: '注册'
+        }
       }
     ]
   },
   {
     path: '/search',
     name: 'Search',
-    component: () => import('../views/Search.vue')
+    component: () => import('../views/Search.vue'),
+    meta: {
+      title: '搜索'
+    }
   },
   {
     path: '/detail/:id',
     name: 'Detail',
-    component: () => import('../views/Detail.vue')
+    component: () => import('../views/Detail.vue'),
+    meta: {
+      title: '商品详情'
+    }
   },
   {
     path: '/cateDetail/:cateId/:subId',
     name: 'CategoryDetail',
-    component: () => import('../views/CategoryDetail.vue')
+    component: () => import('../views/CategoryDetail.vue'),
+    meta: {
+      title: '分类详情'
+    }
   },
   {
     path: '/order/:orderNo',
     name: 'Order',
-    component: () => import('../views/Order.vue')
+    component: () => import('../views/Order.vue'),
+    meta: {
+      title: '订单页面'
+    }
   },
   {
     path: '/myOrder',
     name: 'MyOrder',
-    component: () => import('../views/MyOrder.vue')
+    component: () => import('../views/MyOrder.vue'),
+    meta: {
+      title: '我的订单'
+    }
   }
 ]
 
@@ -71,6 +99,13 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, form, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
 })
 
 export default router
